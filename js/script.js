c92b81a731bfbf27d7ee9a86ab28e15619afc50d
hsmt24 <tuyo@ktj.biglobe.ne.jp>
2016-07-17 03:50:40 +0900

    fix

--- js/script.js
+++ js/script.js
@@ -10,10 +10,7 @@ www.typeandgrids.com
 jQuery.easing.def = "easeOutQuad";
 
 $(document).ready(function()
-{
-	// Hide project info
-	$(".projectInfo").css("display", "none");
-	
+{	
 	// Move projects to second column
 	$(".project:odd").appendTo("#col2");
 		
@@ -24,11 +21,11 @@ $(document).ready(function()
 		
 		$(this).children(".projectThumbnailHover").find("h4").css("display", "block");
 		$(this).children(".projectThumbnailHover").find("h4").css("opacity", "0");
-		$(this).children(".projectThumbnailHover").find("h4").delay(200).animate({left: '30', opacity: 1}, 200);
+		$(this).children(".projectThumbnailHover").find("h4").delay(200).animate({opacity: 1}, 200);
 		
 		$(this).children(".projectThumbnailHover").find("h5").css("display", "block");
 		$(this).children(".projectThumbnailHover").find("h5").css("opacity", "0");
-		$(this).children(".projectThumbnailHover").find("h5").delay(350).animate({left: '30', opacity: 1}, 200);
+		$(this).children(".projectThumbnailHover").find("h5").delay(350).animate({opacity: 1}, 200);
 	})
 	
 	$(".projectThumbnail").on("mouseleave", function(e)
@@ -68,197 +65,6 @@ jQuery(document).ready(function($) {
 	   	directionNavEnabled: false,
 	   	startSlideIndex: 0,
 	   	imageScaleMode: 'fill' },
-		openedProjectInfo,
 		isAnimating = false,
 		currOpenProject;
-
-	function closeOpenedProject(el) {
-		openedProjectInfo.slideUp(900);
-		openedProjectInfo.parent().find('.portfolioSlider').fadeOut();
-		openedProjectInfo = false;
-		if(el && el.length) {
-			el.css('visibility', 'visible');
-		}
-	}
-
-	$(".projectThumbnail").click(function(e) {
-		if(isAnimating) {
-			return;
-		}
-		isAnimating = true;
-		
-		var firstImgLoaded = false,
-			projectEl = $(this).parent('.project'),
-			projectNav = projectEl.find('.projectNav'),
-			
-			//
-			projectInfo = projectEl.find('.projectInfo'),
-			//
-			
-			newOpenProjectInfo = projectEl.find(".projectInfo"),
-			currEl = $(this).find(".thumbnailImage");
-				
-		if( !projectEl.data('slider-inited') ) {
-			var portfolioSliderData = projectEl.find('.portfolioSliderData'),
-				imgPreloaderOverlay;
-		
-			if(portfolioSliderData.length > 0) {
-				imgPreloaderOverlay = $('<div class="first-img-preloader"><div class="preloader-graphics"></d</div>');
-				projectEl.append(imgPreloaderOverlay);
-
-				portfolioSliderData
-					.addClass('portfolioSlidesContainer')
-					.wrap($('<div class="portfolioSlider"></div>'))
-					.find('li').addClass('portfolioSlide');
-			
-				var sliderEl = projectEl.find('.portfolioSlider');
-				currEl.clone().addClass('portfolioImage myImage').appendTo(sliderEl.find('li').eq(0).removeAttr('data-src'));
-				var imgLoadCounter = 0;
-				
-				var sliderInstance = sliderEl.portfolioSlider(sliderProps).data('portfolioSlider');
-				var numSlides = sliderInstance.numSlides;
-				
-				// Fixes bug when resizing window on About page
-				$("#logo, #workPage").click(function() {
-					function bugFix() {
-						sliderInstance.updateSliderSize();
-						$(".projectThumbnailHover").fadeOut(800);
-					}
-					setTimeout(bugFix, 710);
-				});
-				
-				//var currItemCounter = projectNav.find('.projectNavCounter'),
-				var currItemCounter = projectInfo.find('.projectNavCounter'),
-					arrowNext = projectNav.find('.projectNavButtons .next'),
-					arrowPrev = projectNav.find('.projectNavButtons .prev'),
-					arrowPrevBlocked = false,
-					arrowNextBlocked = false;
-
-				function updateNextPrevButtons() {
-					if(sliderInstance.currentSlideId <= 0) {
-						arrowPrev.addClass('projectNavInactive');
-						arrowPrevBlocked = true;
-					} else {
-						arrowPrev.removeClass('projectNavInactive');
-						arrowPrevBlocked = false;
-					}
-
-					if(sliderInstance.currentSlideId >= numSlides - 1) {
-						arrowNext.addClass('projectNavInactive');
-						arrowNextBlocked = true;
-					} else {
-						arrowNext.removeClass('projectNavInactive');
-						arrowNextBlocked = false;
-					}
-				}
-
-				sliderInstance.settings.beforeSlideChange = function() {
-					currItemCounter.text( (sliderInstance.currentSlideId + 1) + ' of ' + numSlides );
-					updateNextPrevButtons();
-				};
-				
-				arrowNext.click(function() {
-					if(!arrowNextBlocked) {
-						sliderInstance.next();
-					}
-				});
-				arrowPrev.click(function() {
-					if(!arrowPrevBlocked) {
-						sliderInstance.prev();
-					}
-				});
-
-				sliderInstance.settings.beforeSlideChange.call();
-				updateNextPrevButtons();
-				projectEl.data('slider-inited', true);
-				
-				imgPreloaderOverlay.css({
-					width: currEl.width(),
-					height: currEl.height()
-				}).fadeIn();
-
-				sliderInstance.settings.imgLoadComplete = function() {
-					imgLoadCounter++;
-
-					if(imgLoadCounter >= 2) {
-						sliderInstance.settings.imgLoadComplete = false;
-						setTimeout(function() {
-							//sliderInstance.updateSliderSize()
-							sliderInstance.goTo(1);
-							isAnimating = false;
-							currEl.css('visibility', 'hidden');
-							imgPreloaderOverlay.stop().fadeOut();
-						}, 400);
-					}
-					
-				};
-
-			} else {
-				if(projectNav.length > 0) {
-					var currItemCounter = projectInfo.find('.projectNavCounter'),
-					arrowNext = projectNav.find('.projectNavButtons .next'),
-					arrowPrev = projectNav.find('.projectNavButtons .prev'),
-					arrowPrevBlocked = false,
-					arrowNextBlocked = false;
-					arrowNext.addClass('projectNavInactive');
-					arrowPrev.addClass('projectNavInactive');
-				}
-				projectEl.data('slider-inited', true);
-				isAnimating = false;
-			}
-		} else {
-			var sliderEl = projectEl.find('.portfolioSlider');
-			if(sliderEl.length > 0) {
-				sliderEl.data('portfolioSlider').goToSilent(0);
-				imgPreloaderOverlay = projectEl.find('.first-img-preloader');
-				imgPreloaderOverlay.css({
-					width: currEl.width(),
-					height: currEl.height()
-				}).fadeIn();
-
-				setTimeout(function() {
-					sliderEl.show();
-					
-					setTimeout(function() {
-						currEl.css({'visibility': 'hidden'});
-						imgPreloaderOverlay.stop().fadeOut();
-						sliderEl.data('portfolioSlider').isAnimating = false;
-						sliderEl.data('portfolioSlider').goTo(1);
-						isAnimating = false;
-					}, 400);
-					
-				}, 450);
-				
-			} else {
-				isAnimating = false;
-			}
-			
-		}
-
-		if(openedProjectInfo) {
-			if(newOpenProjectInfo.is(openedProjectInfo)) {
-				closeOpenedProject(currOpenProject.find(".thumbnailImage"));
-				currOpenProject.find(".projectThumbnailHover").fadeOut(800, function(){currOpenProject.find(".projectThumbnailHover").css("visibility", "visible")});
-				return false;
-			} else {
-				closeOpenedProject(currOpenProject.find(".thumbnailImage"));
-				currOpenProject.find(".projectThumbnailHover").fadeOut(800, function(){currOpenProject.find(".projectThumbnailHover").css("visibility", "visible")});
-			}
-		}
-		currOpenProject =projectEl;
-		openedProjectInfo = newOpenProjectInfo.stop().delay(200).slideDown(900).data('project-open', true);
-		currOpenProject.find(".projectThumbnailHover").fadeOut(200, function(){currOpenProject.find(".projectThumbnailHover").css("visibility", "hidden")});
-	});
-	
-	$(".closeButton").click(function() {
-		
-		// Add a delay to fix weird issue with resizing About page
-		function closeSlider() {
-			closeOpenedProject(currOpenProject.find(".thumbnailImage"));
-			currOpenProject.find(".projectThumbnailHover").css("visibility", "visible");
-		}
-		//setTimeout(closeSlider, 400);
-		setTimeout(closeSlider, 1);
-		
-	});
 });
